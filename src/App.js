084619976537b3a26281fa0978320c89ffa5e761
Malini <Siranjeevi9597536779@gmail.com>
2026-03-26 08:18:25 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import SAPI_P1_Landing from './components/SAPI_P1_Landing';
-import SAPI_P2_preview from './components/SAPI_P2_preview';
-import SAPI_P3_Briefing from './components/SAPI_P3_Briefing';
-import SAPI_P4_DimIntro from './components/SAPI_P4_DimIntro';
-import SAPI_P5_Quiz from './components/SAPI_P5_Quiz';
-import SAPI_P6_Calculating from './components/SAPI_P6_Calculating';
-import SAPI_P7_Results from './components/SAPI_P7_Results';
+import SAPILanding from './components/SAPI_P1_Landing';
+import SAPIPreview from './components/SAPI_P2_preview';
+import SAPIBriefing from './components/SAPI_P3_Briefing';
+import SAPIDimIntro from './components/SAPI_P4_DimIntro';
+import SAPIQuiz from './components/SAPI_P5_Quiz';
+import SAPICalculating from './components/SAPI_P6_Calculating';
+import SAPIResults from './components/SAPI_P7_Results';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -24,13 +24,13 @@ function App() {
       <div className="App">
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<SAPI_P1_Landing />} />
-          <Route path="/preview" element={<SAPI_P2_preview />} />
-          <Route path="/briefing" element={<SAPI_P3_Briefing />} />
-          <Route path="/dimintro" element={<SAPI_P4_DimIntro />} />
-          <Route path="/quiz" element={<SAPI_P5_Quiz />} />
-          <Route path="/calculating" element={<SAPI_P6_Calculating />} />
-          <Route path="/results" element={<SAPI_P7_Results />} />
+          <Route path="/" element={<SAPILanding />} />
+          <Route path="/preview" element={<SAPIPreview />} />
+          <Route path="/briefing" element={<SAPIBriefing />} />
+          <Route path="/dimintro" element={<SAPIDimIntro />} />
+          <Route path="/quiz" element={<SAPIQuiz />} />
+          <Route path="/calculating" element={<SAPICalculating />} />
+          <Route path="/results" element={<SAPIResults />} />
         </Routes>
       </div>
     </Router>
